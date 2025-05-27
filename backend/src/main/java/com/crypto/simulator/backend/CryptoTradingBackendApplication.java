7c9ca54d18564a95644afc49fb2ed2b278752ce8
Kris <kris.nikolov@abv.bg>
2025-05-27 15:53:43 +0300

    Initial commit

---
+++ backend/src/main/java/com/crypto/simulator/backend/CryptoTradingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.simulator.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTradingBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/crypto/simulator/backend/CryptoTradingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.simulator.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
